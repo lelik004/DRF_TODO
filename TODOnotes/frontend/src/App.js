e523f68547f9224368712af751c299d85978c922
Aleksei Abakumov <alekseiabakumov@MacBook-Air-Aleksei.local>
2023-03-21 14:57:20 +0300

    Задания к уроку 5

--- TODOnotes/frontend/src/App.js
+++ TODOnotes/frontend/src/App.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import './App.css';
-import UsersList from "./components/Users";
-import Footer from "./components/Footer";
+import {HashRouter, Route, Routes, Link, BrowserRouter, Switch} from "react-router-dom";
+
 import Menu from "./components/Menu";
+import UsersList from "./components/Users";
+import ProjectsList from "./components/Projects";
+import TodoList from "./components/TodoList";
+import Footer from "./components/Footer";
+import NotFound404 from "./components/NotFound404";
+
 import axios from "axios";
 
 
@@ -11,17 +17,44 @@ class App extends React.Component {
         super(props)
         this.state = {
             'users': [],
-            'footer': []
+            'projects': [],
+            'todo_list': [],
+            'footer': [],
+            'user_detail': [],
         }
     }
 
     componentDidMount() {
+
+        //Users mount
         axios.get('http://127.0.0.1:8000/api/users/')
             .then(response => {
                 const users = response.data
                 this.setState(
                     {
                         'users': users
+                    }
+                )
+            }).catch(error => console.log(error))
+
+        // Projects mount
+        axios.get('http://127.0.0.1:8000/api/projects/')
+            .then(response => {
+                const projects = response.data.results
+                this.setState(
+                    {
+                        'projects': projects
+                    }
+                )
+            }).catch(error => console.log(error))
+
+        // Todo Mount
+        axios.get('http://127.0.0.1:8000/api/todo/')
+            .then(response => {
+                const todo_list = response.data.results
+                this.setState(
+                    {
+                        'todo_list': todo_list
                     }
                 )
             }).catch(error => console.log(error))
@@ -32,10 +65,17 @@ class App extends React.Component {
         return (
             <div>
                 <Menu menu={this.state.menu}/>
-                <UsersList users={this.state.users}/>
+                <BrowserRouter>
+                    <Switch>
+                        <Route exact path='/users' component={() => <UsersList users={this.state.users}/>}/>
+                        <Route exact path='/projects' component={() => <ProjectsList projects={this.state.projects}/>}/>
+                        <Route exact path='/todo' component={() => <TodoList todo_list={this.state.todo_list}/>}/>
+                        {/*<Route component={NotFound404}/>*/}
+                    </Switch>
+                </BrowserRouter>
                 <Footer footer={this.state.footer}/>
             </div>
-    )
+        )
     }
 }
 
